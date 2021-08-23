867e9e1f394e045571fb0c01c2da0f34a829c5bf
Lloyd Schneck <lloydschneck@lloyds-mbp.lan>
2021-08-23 16:38:13 +0930

    some edits to my original submission

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <div className='decorative-line'>
+
       <CharacterEditor />
       <Footer />
-      </div>
+    
     </>
   );
 }
