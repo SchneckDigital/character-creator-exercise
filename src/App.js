bf22931dfb0202e64a38efce738d43c7f0e44c92
Lloyd Schneck <lloydschneck@lloyds-mbp.lan>
2021-08-23 15:49:10 +0930

    Adding character creator to repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <div className='decorative-line'>
+      <CharacterEditor />
+      <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      All character assets taken from{' '}
+      <a
+      className={styles.anchor}
+        href="https://www.openpeeps.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open Peeps
+      </a>
+      , by Pablo Stanley
+    </footer>
+  );
+};
+
+export default Footer;
